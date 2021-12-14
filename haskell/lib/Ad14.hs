4c3d6a5516439236c647d0628e4d8d28822659b8
Owen Shepherd <owen@owen.cafe>
2021-12-14 21:08:08 +0000

    clean up day 14

--- haskell/lib/Ad14.hs
+++ haskell/lib/Ad14.hs
@@ -11,11 +11,11 @@ import Data.Tuple
 import Data.Char
 import Data.Functor
 
-type Path = (String, Char)
+type Path = ((Char, Char), Char)
 type Input = (String, [Path])
 
 readPath :: String -> Path
-readPath (c1 : c2 : ' ' : '-' : '>' : ' ' : c3 : []) = ([c1, c2], c3)
+readPath (c1 : c2 : ' ' : '-' : '>' : ' ' : c3 : []) = ((c1, c2), c3)
 
 windows' :: Int -> [a] -> [[a]]
 windows' n [] = []
@@ -44,29 +44,29 @@ addOcc (s, o) ((s1, o1) : xs)
   | s == s1 = (s, o1 + o) : xs
   | otherwise = (s1, o1) : addOcc (s, o) xs
 
-step :: [Path] -> [Occ String] -> [Occ String]
-step paths ((x1@[a, b], c) : xs) =
+step :: [Path] -> [Occ (Char, Char)] -> [Occ (Char, Char)]
+step paths ((x1@(a, b), c) : xs) =
   let out = fromJust $ lookup x1 paths
       re = step paths xs
-  in addOcc ([a, out], c) $ addOcc ([out, b], c) re
-step paths [] = []
+  in addOcc ((a, out), c) $ addOcc ((out, b), c) re
+step _ [] = []
 
-chains :: [Path] -> [Occ String] -> [[Occ String]]
+chains :: [Path] -> [Occ (Char, Char)] -> [[Occ (Char, Char)]]
 chains paths start = start : chains paths (step paths start)
 
-tupOccs :: String -> [(String, Int)]
-tupOccs xs = 
-  let w = windows 2 xs
+tupOccs :: String -> [((Char, Char), Int)]
+tupOccs xs =
+  let w = (\[a, b] -> (a, b)) <$> windows 2 xs
       u = nub w
   in u <&> \tup -> (tup, count tup w)
- 
+
 solve :: Int -> Input -> Int
 solve iters (start, paths) =
   let occs = tupOccs start
       end = chains paths occs !! iters
       counts = addOcc (head start, 1)
         $ addOcc (last start, 1)
-        $ foldl' (flip addOcc) [] ((end >>= \(s, c) -> (,c) <$> s) :: [Occ Char])
+        $ foldl' (flip addOcc) [] ((end >>= \((c1, c2), c) -> (,c) <$> [c1, c2]) :: [Occ Char])
       ma = maximum $ snd <$> counts
       mi = minimum $ snd <$> counts
   in (ma `div` 2) - (mi `div` 2)
