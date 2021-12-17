f2c5e7cdcccdf362849d5e7ee4c9e7da39894fe8
Owen Shepherd <owen@owen.cafe>
2021-12-17 12:04:46 +0000

    upgrade nixpkgs, add hlint, hls

--- default.nix
+++ default.nix
@@ -1,10 +1,23 @@
-{ mkDerivation, base, lib, containers, mtl, vector, dlist, criterion, heaps }:
+{ mkDerivation
+, base
+, hlint
+, haskell-language-server
+, lib
+, containers
+, mtl
+, vector
+, dlist
+, criterion
+, heaps
+}:
+
 mkDerivation {
   pname = "advent";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [ base containers mtl vector dlist criterion heaps ];
   testHaskellDepends = [ base ];
+  buildTools = [ hlint haskell-language-server ];
   description = "Advent of Code solutions";
   license = lib.licenses.mit;
 }
